2e83a88fd325db2be8bcc58504ddd5ab66768198
Marek Vozarik <marekvozarik@yahoo.com>
2018-12-08 19:03:37 +0100

    style upgrade v2

--- src/Item.js
+++ src/Item.js
@@ -2,15 +2,41 @@ import './App.css';
 import React, { Component } from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Button from '@material-ui/core/Button';
 
 class SingleItem extends Component {
+
+    constructor(props){
+        super(props);
+    
+        this.state = {
+            chcboxValue: true
+        };
+    
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+
+    handleChange(){
+        this.setState({chcboxValue: !this.state.chcboxValue});
+    }
+
     render() {
         return(
             <div>
                 <FormControlLabel className="item"
-                    control={<Checkbox/>}
-                    label={this.props.label}
+                    control={
+                        <div>
+                            <Checkbox checked={this.state.chcboxValue}/>
+                            <Button onClick={this.handleChange}>
+                                {this.props.label}
+                            </Button>
+                        </div>
+                    
+                }
+                    // label={this.props.label}
                 />
+                
             </div>
         )
     }
