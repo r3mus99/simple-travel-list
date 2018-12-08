a9965b5105b934b46a784a0d4f274cf67004e734
Marek Vozarik <marekvozarik@yahoo.com>
2018-12-08 20:05:24 +0100

    visibility icon

--- src/Item.js
+++ src/Item.js
@@ -2,6 +2,9 @@ import './App.css';
 import React, { Component } from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import VisibilityOn from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 class SingleItem extends Component {
 
@@ -9,24 +12,46 @@ class SingleItem extends Component {
         super(props);
     
         this.state = {
-            chcboxValue: true
+            checkBoxValue: false,
+            visibility: false
         };
     
+        // todo refactor
         this.handleChange = this.handleChange.bind(this);
+        this.handleVisibility = this.handleVisibility.bind(this);
     }
 
+    handleChange() {
+        this.setState({checkBoxValue: !this.state.checkBoxValue});
+    }
 
-    handleChange(){
-        this.setState({chcboxValue: !this.state.chcboxValue});
+    handleVisibility() {
+        this.setState({visibility: !this.state.visibility});
     }
 
     render() {
+        let visibilityButton;
+        if (this.state.visibility) {
+            visibilityButton = <VisibilityOn/>;
+        } else {
+            visibilityButton = <VisibilityOff/>;
+        }
+
+
         return(
             <div className="item">
-                <Checkbox checked={this.state.chcboxValue}/>
-                <Button onClick={this.handleChange}>
+                <Checkbox 
+                    checked={this.state.checkBoxValue}
+                    onClick={this.handleChange}/>
+                <Button 
+                    onClick={this.handleChange}>
                     {this.props.label}
                 </Button>
+                <IconButton aria-label="hide" className="grayIcon"
+                    onClick={this.handleVisibility}>
+                    {visibilityButton}
+                </IconButton>
+                
             </div>
         )
     }
