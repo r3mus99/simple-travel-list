e50ad7910a27eb4db5fc932acdda999d7f920aaf
Marek Vozarik <marekvozarik@yahoo.com>
2018-12-08 18:25:42 +0100

    upgrade

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
-import SingleItem from './SingleItem';
+import Item from './Item';
 
 class App extends Component {
   render() {
+
+    // todo refactor
+    const ColoredLine = ({ color }) => (
+      <hr
+          style={{
+              color: color,
+              backgroundColor: color,
+              height: 1
+          }}
+      />
+    );
+
     return (
       <div className="App">
-        <SingleItem label="občianský preukaz"/>
-        <SingleItem label="cestovný pas"/>
-        <SingleItem label="kartička cestovného poistenia"/>
-        <SingleItem label="kartička zdravotného poistenia"/>
+        <div className="Section">
+          <p className="Header1">Hlavné</p>
+          <Item label="peniaze a platobná karta"/>
+          <Item label="občianský preukaz"/>
+          <Item label="vodičský preukaz"/>
+          <Item label="cestovný pas"/>
+          <Item label="kartička cestovného poistenia"/>
+          <Item label="kartička zdravotného poistenia"/>
+          <Item label="dokumenty od cestovnej kancelárie"/>
+          <ColoredLine color="gainsboro" />
+          <Item label="Lieky, ktoré beriem"/>
+          <Item label="Lieky, ktoré by sa mohli hodiť"/>
+          <ColoredLine color="gainsboro" />
+          <Item label="Okuliare"/>
+          <Item label="Telefón (+ nabíjačka)"/>
+          <Item label="zámky na batožinu (+ kľúče)"/>
+        </div>
+        <div className="Section">
+          <p className="Header1">Oblečenie</p>
+          <Item label="košele"/>
+          <Item label="nohavice"/>
+          <Item label="oblek a kravata"/>
+          <Item label="plavky"/>
+        </div>
       </div>
-      
     );
   }
 }
--- src/Item.js
+++ src/Item.js
@@ -1,21 +1,19 @@
+import './App.css';
 import React, { Component } from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 class SingleItem extends Component {
-
-
     render() {
         return(
             <div>
-                <FormControlLabel
+                <FormControlLabel className="item"
                     control={<Checkbox/>}
                     label={this.props.label}
                 />
             </div>
         )
     }
-
 }
 
 export default SingleItem;
