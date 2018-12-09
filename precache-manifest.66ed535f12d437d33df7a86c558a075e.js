f2a2b4ba40de9e99622d99b6f439e060844d1a57
Marek Vozarik <marekvozarik@yahoo.com>
2018-12-09 09:07:48 +0100

    Updates

---
+++ precache-manifest.66ed535f12d437d33df7a86c558a075e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afd2e990e21c0e817bec",
+    "url": "/zoznam-na-cestu/static/js/runtime~main.afd2e990.js"
+  },
+  {
+    "revision": "c940f85abbaf590310c7",
+    "url": "/zoznam-na-cestu/static/js/main.c940f85a.chunk.js"
+  },
+  {
+    "revision": "b07f6615a20fa5fd59c2",
+    "url": "/zoznam-na-cestu/static/js/1.b07f6615.chunk.js"
+  },
+  {
+    "revision": "c940f85abbaf590310c7",
+    "url": "/zoznam-na-cestu/static/css/main.79a65b9e.chunk.css"
+  },
+  {
+    "revision": "ce0a90a55f656665e0e39527ab6fdfbc",
+    "url": "/zoznam-na-cestu/index.html"
+  }
+];
